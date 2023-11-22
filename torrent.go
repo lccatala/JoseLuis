947065e9e400e166aac36be88b50b42f6640c81e
lccatala <alpasfly@gmail.com>
2023-11-22 22:24:43 +0100

    Communicating with other peers

--- torrent.go
+++ torrent.go
@@ -3,8 +3,12 @@ package main
 import (
 	"bytes"
 	"crypto/sha1"
+	"encoding/binary"
 	"fmt"
 	"io"
+	"net"
+	"net/url"
+	"strconv"
 
 	"github.com/jackpal/bencode-go"
 )
@@ -88,4 +92,46 @@ func (bto bencodeTorrent) toTorrentFile() (TorrentFile, error) {
 		PieceHashes: pieceHashes,
 	}
 	return torrentFile, nil
+}
+
+func (t *TorrentFile) buildTrackerURL(peerID [20]byte, port uint16) (string, error) {
+	base, err := url.Parse(t.Announce)
+	if err != nil {
+		return "", err
+	}
+
+	params := url.Values{
+		"info_hash":  []string{string(t.InfoHash[:])},
+		"peer_id":    []string{string(peerID[:])},
+		"port":       []string{strconv.Itoa(int(port))},
+		"uploaded":   []string{"0"},
+		"downloaded": []string{"0"},
+		"compact":    []string{"1"},
+		"left":       []string{strconv.Itoa(t.Length)},
+	}
+
+	base.RawQuery = params.Encode()
+	return base.String(), nil
+}
+
+type Peer struct {
+	IP   net.IP
+	Port uint16
+}
+
+// Unmarshal parses peer IP addresses and ports from a buffer
+func Unmarshal(peersBin []byte) ([]Peer, error) {
+	const peerSize = 6 // 4 for IP, 2 for port
+	numPeers := len(peersBin)
+	if len(peersBin)%peerSize != 0 {
+		err := fmt.Errorf("Received malformed peers")
+		return nil, err
+	}
+	peers := make([]Peer, numPeers)
+	for i := 0; i < numPeers; i++ {
+		offset := i + peerSize
+		peers[i].IP = net.IP(peersBin[offset : offset+4])
+		peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4 : offset+6])
+	}
+	return peers, nil
 }
