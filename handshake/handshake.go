98e9393399409e67d2aa2946076715a995bf6bf2
lccatala <alpasfly@gmail.com>
2023-11-23 22:49:15 +0100

    Refactor so it builds since there is no function overloading in this piece of shit language or so it seems

---
+++ handshake/handshake.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+type Handshake struct {
+	Pstr     string
+	InfoHash [20]byte
+	PeerID   [20]byte
+}
+
+// Serialize serializes the handshake to a buffer
+func (h *Handshake) Serialize() []byte {
+	buf := make([]byte, len(h.Pstr)+49)
+	buf[0] = byte(len(h.Pstr))
+	curr := 1
+	curr += copy(buf[curr:], []byte(h.Pstr))
+	curr += copy(buf[curr:], make([]byte, 8)) // 8 reserved bytes
+	curr += copy(buf[curr:], h.InfoHash[:])
+	curr += copy(buf[curr:], h.PeerID[:])
+	return buf
+}
+
+// Read parses a handshake from a stream
+func Read(r io.Reader) (*Handshake, error) {
+	lengthBuf := make([]byte, 1)
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	pstrLen := int(lengthBuf[0])
+	if pstrLen == 0 {
+		err := fmt.Errorf("Pstr length cannot be 0")
+		return nil, err
+	}
+
+	handshakeBuf := make([]byte, 48+pstrLen)
+	_, err = io.ReadFull(r, handshakeBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	var infoHash, peerID [20]byte
+	copy(infoHash[:], handshakeBuf[pstrLen+8:pstrLen+8+20])
+	copy(peerID[:], handshakeBuf[pstrLen+8+20:])
+
+	h := Handshake{
+		Pstr:     string(handshakeBuf[0:pstrLen]),
+		InfoHash: infoHash,
+		PeerID:   peerID,
+	}
+
+	return &h, nil
+}
--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	filename := "/Users/luis/Downloads/debian-12.2.0-amd64-netinst.iso.torrent"
-	f, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("Cannot read file %q", filename)
-		os.Exit(-1)
-	}
-
-	torrent, err := Open(f)
-	torrentFile, err := torrent.toTorrentFile()
-	if err != nil {
-		fmt.Printf("Cannot convert to torrent file")
-		os.Exit(-1)
-	}
-	fmt.Printf("Announce is: %q\n", torrentFile.Announce)
+	// peer := Peer{}
+	// conn, err := net.DialTimeout("tcp", peer.IP.String(), 3*time.Second)
+	// if err != nil {
+	// 	fmt.Errorf("Could not start TCP connection: %q", err)
+	// 	os.Exit(-1)
+	// }
+	// filename := "/Users/luis/Downloads/debian-12.2.0-amd64-netinst.iso.torrent"
+	// f, err := os.Open(filename)
+	// if err != nil {
+	// 	fmt.Printf("Cannot read file %q", filename)
+	// 	os.Exit(-1)
+	// }
+	//
+	// torrent, err := Open(f)
+	// torrentFile, err := torrent.toTorrentFile()
+	// if err != nil {
+	// 	fmt.Printf("Cannot convert to torrent file")
+	// 	os.Exit(-1)
+	// }
+	// fmt.Printf("Announce is: %q\n", torrentFile.Announce)
 
 }
