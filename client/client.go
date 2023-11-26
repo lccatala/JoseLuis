2832f51990591fc3b0359e73612790b876886c9f
lccatala <alpasfly@gmail.com>
2023-11-27 00:08:53 +0100

    Add peer client code

---
+++ client/client.go
@@ -0,0 +1,120 @@
+package client
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/lccatala/JoseLuis/bitfield"
+	"github.com/lccatala/JoseLuis/handshake"
+	"github.com/lccatala/JoseLuis/message"
+	"github.com/lccatala/JoseLuis/peers"
+)
+
+type Client struct {
+	Conn     net.Conn
+	Choked   bool
+	Bitfield bitfield.Bitfield
+	peer     peers.Peer
+	infoHash [20]byte
+	peerID   [20]byte
+}
+
+func completeHandshake(conn net.Conn, infoHash, peerID [20]byte) (*handshake.Handshake, error) {
+	conn.SetDeadline(time.Now().Add(3 * time.Second))
+	defer conn.SetDeadline(time.Time{}) // Disable the deadline
+
+	req := handshake.New(infoHash, peerID)
+	_, err := conn.Write(req.Serialize())
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := handshake.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if !bytes.Equal(res.InfoHash[:], infoHash[:]) {
+		return nil, fmt.Errorf("Expected infohash %x but got %x", res.InfoHash, infoHash)
+	}
+
+	return res, nil
+}
+
+// receiveBitfield gets the payload from the message of a connection
+// returns an err if the message can't be Read or if the ID is not of type message.MsgBifield
+func receiveBitfield(conn net.Conn) (bitfield.Bitfield, error) {
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{}) // Disable the deadline
+
+	msg, err := message.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if msg.ID != message.MsgBitfield {
+		err := fmt.Errorf("Expected bitfield but got ID %d", msg.ID)
+		return nil, err
+	}
+
+	return msg.Payload, nil
+}
+
+// New connects with a peer, completes a handshake and receives a handshake
+// returns an err if any of those fail
+func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
+	conn, err := net.DialTimeout("tcp", peer.String(), 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = completeHandshake(conn, infoHash, peerID)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	bitfield, err := receiveBitfield(conn)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return &Client{
+		Conn:     conn,
+		Choked:   true,
+		Bitfield: bitfield,
+		peer:     peer,
+		infoHash: infoHash,
+		peerID:   peerID,
+	}, nil
+}
+
+// SendRequest sends a Request message to the peer
+func (c *Client) SendRequest(index, begin, length int) error {
+	req := message.FormatRequest(index, begin, length)
+	_, err := c.Conn.Write(req.Serialize())
+	return err
+}
+
+// SendInterested sends a Interested message to the peer
+func (c *Client) SendInterested() error {
+	msg := message.Message{ID: message.MsgInterested}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+// SendUnchoke sends an Unchoke message to the peer
+func (c *Client) SendUnchoke() error {
+	msg := message.Message{ID: message.MsgUnchoke}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+// SendHave sends a Have message to the peer
+func (c *Client) SendHave() error {
+	msg := message.Message{ID: message.MsgHave}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
